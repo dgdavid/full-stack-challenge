e4dc4c572898afa1bebc2e982e56b20359b6b829
David Díaz <diazglezdavid@gmail.com>
2017-08-16 21:20:39 +0100

    Remove some unused gems
    For the purpuose of this project, some included by default gems are
    not necessaries. Even so, is still needed a more deeper cleanup.
    
    Also moves the .editorconfig file to `/main` directory.

--- main/config/environment.rb
+++ main/config/environment.rb
@@ -1,6 +1,5 @@
 require 'bundler/setup'
 require 'hanami/setup'
-require 'hanami/model'
 require_relative '../lib/main'
 require_relative '../apps/api/application'
 require_relative '../apps/front/application'
@@ -8,33 +7,6 @@ require_relative '../apps/front/application'
 Hanami.configure do
   mount Api::Application, at: '/api'
   mount Front::Application, at: '/'
-
-  model do
-    ##
-    # Database adapter
-    #
-    # Available options:
-    #
-    #  * SQL adapter
-    #    adapter :sql, 'sqlite://db/main_development.sqlite3'
-    #    adapter :sql, 'postgresql://localhost/main_development'
-    #    adapter :sql, 'mysql://localhost/main_development'
-    #
-    adapter :sql, ENV['DATABASE_URL']
-
-    ##
-    # Migrations
-    #
-    migrations 'db/migrations'
-    schema     'db/schema.sql'
-  end
-
-  mailer do
-    root 'lib/main/mailers'
-
-    # See http://hanamirb.org/guides/mailers/delivery
-    delivery :test
-  end
 
   environment :development do
     # See: http://hanamirb.org/guides/projects/logging
