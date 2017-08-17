b9ec2885921ac4bafc83bfc8591a8bfeda5ee3af
David Díaz <diazglezdavid@gmail.com>
2017-08-17 20:42:47 +0100

    [Preact Client] Request city information to Hanami/API

--- preact_client/src/routes/city_information/index.js
+++ preact_client/src/routes/city_information/index.js
@@ -1,4 +1,5 @@
 import { h, Component } from 'preact';
+import axios from 'axios';
 
 /**
  * Component responsible to request and render information
@@ -9,17 +10,23 @@ export default class CityInformation extends Component {
     country: undefined,
     currency: undefined,
     temperature: undefined,
-    weatherCode: undefined
+    condition_code: undefined // For simplicity reasons the key from api is kept
   };
 
   componentWillMount() {
     this.setState({ name: this.props.name });
+
+    axios.get(`http://localhost:2300/api/city/${this.props.name}`)
+      .then(response => this.setState(response.data))
+      .catch(error => {
+        // Log error only in development mode
+      });
   }
 
-  render({}, { name, country, currency, temperature, weatherCode }) {
+  render({}, { name, country, currency, temperature, condition_code }) {
     return (
       <div class="wrapper">
-        <i class={`weather-condition c${weatherCode}`}></i>
+        <i class={`weather-condition c${condition_code}`}></i>
         <div class="city-information">
           <span class="city-name">{ name }, </span>
           <span class="country-name">{ country }</span>
